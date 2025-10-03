550c0c46c6444e9037bdcbbe1ecd292d4d170c4c
ifoodplayer <joaogamer1437@gmail.com>
2025-10-03 15:12:05 -0300

    Import: Prima wifi driver

--- build_kernel.sh
+++ build_kernel.sh
@@ -9,3 +9,4 @@ make -C $(pwd) O=$(pwd)/out KCFLAGS=-mno-android samsung/m11q_eur_open_defconfig
 make -j16 -C $(pwd) O=$(pwd)/out KCFLAGS=-mno-android DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y
  
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+aarch64-linux-gnu-strip --strip-unneeded --strip-debug out/drivers/staging/prima/wlan.ko && cp -rf out/drivers/staging/prima/wlan.ko out/drivers/staging/prima/pronto_wlan.ko
